b8ed921b02a9c1c2d6a69470a0c7da40c1d25f05
TakacsRobert <ttrob77@gmail.com>
2022-11-16 23:23:43 +0100

    Made a spawnable and destroyable object on the network

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,49 @@ using UnityEngine.InputSystem;
 using UnityEngine.AI;
 using System;
 using Unity.Netcode;
+using Unity.Collections;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerController : NetworkBehaviour
 {
-
+    [SerializeField] private Transform spawnedObjectPrefab;
     [SerializeField] private InputAction movement = new InputAction();
     [SerializeField] private LayerMask layerMask = new LayerMask();
     private NavMeshAgent agent = null;
     private Camera cam = null;
+    private Transform spawnedObjectTransform;
+
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData
+        {
+            _int = 56,
+            _bool = true,
+        },
+        NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+
+
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) =>
+        {
+            Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
+        };
+    }
 
     private void Start()
     {
@@ -36,6 +70,17 @@ public class PlayerController : NetworkBehaviour
     private void Update()
     {
         if (!IsOwner) return;
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            spawnedObjectTransform = Instantiate(spawnedObjectPrefab);
+            spawnedObjectTransform.GetComponent<NetworkObject>().Spawn(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            Destroy(spawnedObjectTransform.gameObject);
+        }
 
         HandleInput();
     }
