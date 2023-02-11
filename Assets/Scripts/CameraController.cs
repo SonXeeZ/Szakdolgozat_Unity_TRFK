55920fadb37f71de7f7f06524788f69550cfd0fd
CHRiS <sonxe@hotmail.hu>
2023-02-11 23:29:50 +0100

    Added player counter text

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using Unity.Netcode;
 
 public class CameraController : NetworkBehaviour
-{
+{ // -- FK --
     [SerializeField] private Transform target = null; // target for the camera to Look At.
     [SerializeField] private Vector3 offset = new Vector3(); // Camera distance compared to the player
     [SerializeField] private float pitch = 2f; // Camera distance from player feet upwards
--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode;
+using TMPro;
 
 public class NetworkManagerUI : MonoBehaviour
-{
+{ // -- TR -- 
     [SerializeField] private Button serverBtn;
     [SerializeField] private Button hostBtn;
     [SerializeField] private Button clientBtn;
 
+    [SerializeField] private TextMeshProUGUI playersIngameText; // -- FK -- 2023.02.11
+
     private void Awake()
     {
+        Cursor.visible = true; // -- FK -- 2023.02.11
+
         serverBtn.onClick.AddListener(() =>
         {
             NetworkManager.Singleton.StartServer();
@@ -24,6 +29,12 @@ public class NetworkManagerUI : MonoBehaviour
         {
             NetworkManager.Singleton.StartClient();
         });
+    }
+    // https://www.youtube.com/watch?v=rFCFMkzFaog&list=PLQMQNmwN3FvyyeI1-bDcBPmZiSaDMbFTi&index=2 -- FK -- 2023.02.11
+
+    private void Update() // PlayersManager Update Kapcsolódik hozzá. -- FK -- 2023.02.11 https://www.youtube.com/watch?v=HWPKlpeZUjM&t=895s
+    {
+        playersIngameText.text = $"Players in game: {PlayersManager.Instance.GetPlayersInGame().ToString()}";
     }
 
 }
