6ffc9bf9738b528ae87bdcac17dfc8e809841bec
CHRiS <sonxe@hotmail.hu>
2023-03-24 19:03:13 +0100

    Created map for Login Screen and Fixed CameraWobble Nullreference error.

--- Assets/Scripts/Accountvalidations/CameraWobble.cs
+++ Assets/Scripts/Accountvalidations/CameraWobble.cs
@@ -6,12 +6,12 @@ public class CameraWobble : MonoBehaviour
 { // --FK-- 2023.02.08
         public float wobbleAmount = 0.7f;
 
-    [SerializeField] public Transform currentCamera = null;
+    private Camera currentCamera;
     Vector3 m_cameraPos;
 
-    void Start()
+    void Awake()
     {
-        currentCamera = Camera.main.transform;
+        currentCamera = Camera.main;
         m_cameraPos = currentCamera.transform.position;
     }
 
@@ -19,11 +19,11 @@ public class CameraWobble : MonoBehaviour
        
         /// szinusz függvény
         //https://gist.github.com/ftvs/5822103
-        currentCamera.localPosition = m_cameraPos + new Vector3(0, Mathf.Sin(Time.time * 2) * wobbleAmount, 0);
+        currentCamera.transform.localPosition = m_cameraPos + new Vector3(0, Mathf.Sin(Time.time * 2) * wobbleAmount, 0);
     }
 
     void OnEnable(){
-        m_cameraPos = currentCamera.localPosition;
+        m_cameraPos = currentCamera.transform.localPosition;
     }
 
 
